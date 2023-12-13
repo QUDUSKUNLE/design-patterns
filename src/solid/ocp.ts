c5ff01f20217882def1902759e29727225aaa3ad
QUDUSKUNLE <quduskunle@gmail.com>
2023-12-13 16:26:34 +0100

    Open Closed Principle

---
+++ src/solid/ocp.ts
@@ -0,0 +1,52 @@
+// Open/Closed Principle
+/**
+ * 
+ * Software entities (classes, modules, functions) should be open
+ * for extension but closed for modification.
+ * You should be able to add new functionality without changing existing code.
+ */
+
+// Before OCP
+
+class Area {
+  width!: number;
+  height!: number;
+}
+
+class AreaCalculator {
+  calculateArea(rectangle: Area) {
+    return rectangle.width * rectangle.height;
+  }
+}
+
+
+// After OCP
+interface Shape {
+  calculateArea(): number;
+  calculatePerimeter(): number;
+}
+
+class Reactangle implements Shape {
+  height!: number;
+  width!: number;
+  calculateArea(): number {
+    return this.height * this.width;
+  }
+
+  calculatePerimeter(): number {
+    return 2 * (this.height + this.width);
+  }
+}
+
+
+class Circle implements Shape {
+  radius!: number;
+
+  calculateArea(): number {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+
+  calculatePerimeter(): number {
+    return 2 * Math.PI * this.radius;
+  }
+}
