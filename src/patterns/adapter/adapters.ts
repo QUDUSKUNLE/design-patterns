8feba89eac47fb5ee3b6c3e738c02f0e9c1394a4
QUDUSKUNLE <quduskunle@gmail.com>
2024-09-02 12:26:42 +0100

    Implement Adapter Patterns

--- src/patterns/adapter/adapters.ts
+++ src/patterns/adapter/adapters.ts
@@ -3,38 +3,6 @@
  * 
  */
 
-interface Iphone {
-  useLightning(): void;
-}
-
-interface Android {
-  useMicroUSB(): void
-}
-
-class Iphones implements Iphone {
-  useLightning(): void {
-    throw new Error("Method not implemented.");
-  }
-}
-
-class Androids implements Android {
-  useMicroUSB(): void {
-    throw new Error("Method not implemented.");
-  }
-}
-
-class LightningToMicroUSBAdapter implements Android {
-  iphoneDevice: Iphone;
-  
-  constructor(iphone: Iphone) {
-    this.iphoneDevice = iphone;
-  }
-
-  public useMicroUSB(): void {
-    throw new Error("Method not implemented.");
-  }
-}
-
 /** 1
  * Define an abstract interface for each object
  * An example, we have two kind of objects
